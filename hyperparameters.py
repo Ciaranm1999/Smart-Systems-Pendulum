7fe9f9cac1b18a6943bdca4db1b5478539dd211d
Calvin <calvinbootsman@hotmail.com>
2025-03-31 23:26:44 +0200

    improved hyperparameters

--- hyperparameters.py
+++ hyperparameters.py
@@ -5,7 +5,7 @@ class hyperparameters:
     # reinformcement replay buffer class
     LEARNING_RATE = 1e-4
     EPSILON_START = 0.9
-    EPSILON_DECAY = 0.999
+    EPSILON_DECAY = 0.9999
     EPSILON_MIN = 0.05
 
     # reinforcement pendulum class
@@ -14,17 +14,17 @@ class hyperparameters:
     TAU = 0.001
     Q_MAX_VALUE = 40
     Q_MIN_VALUE = -40
-    REWARDS_SCALING = 1/15
+    REWARDS_SCALING = 1/50
     # Lab 1 
     MAX_RUN_TIME = 60.0 # seconds
-    SAMPLE_TIME = 0.05 # seconds
+    SAMPLE_TIME = 0.5 # seconds
     EPOCHS_MAX = 50000
-    MAX_POSITION = 350
+    MAX_POSITION = 0.6 # meters
 
     PRINT_OUTPUT = False
-    RENDER = True
+    RENDER = False
     SCALE = 1
-    MANUAL_MODE = True
+    MANUAL_MODE = False
 
     # motor class
     MOTOR_R = 1.5  # Resistance in ohms
@@ -32,7 +32,7 @@ class hyperparameters:
     MOTOR_K_e = 0.00375 * MOTOR_R  # Back EMF constant in V/(rad/s)
     MOTOR_J = 9e-5  # Rotor inertia in kg.m^2
     MOTOR_B = 1e-6  # Viscous friction coefficient in Nm.s/rad
-    MOTOR_V_MAX = 12.0  # Maximum voltage in volt
+    MOTOR_V_MAX = 4.0  # Maximum voltage in volt
     PULLY_RADIUS = 0.01 # 1 cm
 
     PENDULUM_LENGHT = 0.35  # Length of the pendulum in meters
