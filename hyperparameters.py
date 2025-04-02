5cb4882fc5b3b89ec5bb2e4b0f3060f7de0f7a80
Calvin Bootsman <calvinbootsman@hotmail.com>
2025-04-02 20:21:31 +0200

    updated hyperparameters

--- hyperparameters.py
+++ hyperparameters.py
@@ -8,16 +8,16 @@ class hyperparameters:
     COLOUMB_FRICTION = 0.02  # Coefficient of Coulomb friction
     AIR_FRICTION = 0.001  # Coefficient of air friction
 
-    TRACK_LENGTH = 0.5 # Length of the track in meters
+    TRACK_LENGTH = 0.27 # Length of the track in meters
     TRACK_DRAW_LENGTH = 300 # Length of the track in pixels
 
     # motor class
-    MOTOR_R =1.5  # Resistance in ohms
+    MOTOR_R =7  # Resistance in ohms
     MOTOR_K_t = 0.00375 * MOTOR_R  /2# Torque constant in Nm/A
     MOTOR_K_e = 0.00375 * MOTOR_R  /2# Back EMF constant in V/(rad/s)
     MOTOR_J = 9e-5  # Rotor inertia in kg.m^2
     MOTOR_B = 1e-6  # Viscous friction coefficient in Nm.s/rad
-    MOTOR_V_MAX = 6.0  # Maximum voltage in volt
-    PULLY_RADIUS = 0.01 # 1 cm
+    MOTOR_V_MAX = 12.0  # Maximum voltage in volt
+    PULLY_RADIUS = 0.005 # 1 cm
 
-    
+    range_encoder = 163
