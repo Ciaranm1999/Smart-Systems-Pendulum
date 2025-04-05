ceb4e512a8854e72bb7f664bc065767bb1e2bf63
Calvin <calvinbootsman@hotmail.com>
2025-04-05 13:18:01 +0200

    updated run_actionlist

--- run_actionlist.py
+++ run_actionlist.py
@@ -5,20 +5,22 @@ from Digital_twin import DigitalTwin
 # Before starting run pip install -r requirements.txt
 
 digital_twin = DigitalTwin()
-        
+
 if __name__=='__main__':
         running = True
         # You can test a sequence of actions (find the action map in the digitalTwin).
         # Each action is performed after 200ms so that the actions do not overlap in time.
         # Can also use your keyboard to manually control the system.
-        actions =  [7, 2, 0, 4, 7, 5, 3, 8, 4, 0]
- 
+
+        actions =  [ 4, 5, 8, 2, 5, 6, 4, 8, 6, 7, 2, 6, 1, 7, 3, 7, 3, 7, 4, 7, 3, 8, 4, 7, 7, 3, 5, 4, 7, 4, 8, 7, 2, 6, 4, 8, 4, 6, 6, 3 ]
+        test = 0.5
+        steps_time = test / digital_twin.delta_t
         #digital_twin.connect_device()
         #digital_twin.start_recording("test_data_3")
         while running:
             # Perform action after 200ms > one step in the dt simulation is delta_t,  
             # in this case 0.005 seconds or 5ms.
-            if digital_twin.steps%40 == 0 and len(actions) > 0:
+            if digital_twin.steps%steps_time == 0 and len(actions) > 0:
                 action = actions.pop(0)
                 direction, duration = digital_twin.action_map[action]
                 digital_twin.perform_action(direction, duration)
